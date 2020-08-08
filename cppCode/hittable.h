d10924d40ea678b70c48de5913fb2520e8fa1ddc
YanjieZe <59699800+theLastRefugee@users.noreply.github.com>
2020-08-08 20:18:21 +0800

    Add files via upload

---
+++ cppCode/hittable.h
@@ -0,0 +1,28 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+#include "rtweekend.h"
+
+class material;
+
+struct hit_record {
+    vec3 p;
+    vec3 normal;
+    shared_ptr<material> mat_ptr;
+    double t;
+    bool front_face;
+
+    //判断ray是在外面还是在里面，并将normal与ray方向相反
+    inline void set_face_normal(const ray& r, const vec3& outward_normal) {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal :-outward_normal;
+    }
+};
+
+class hittable {
+    public:
+        virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const = 0;
+};
+
+#endif
